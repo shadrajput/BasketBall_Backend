37fef7c2018356c5095271b36b18a2d310d16f36
SadikAli-Karadiya <karadiyasadikali@gmail.com>
2023-03-24 11:02:33 +0530

    player, team and tournament delete API created

--- src/routes/admin/admin.controller.js
+++ src/routes/admin/admin.controller.js
@@ -60,8 +60,157 @@ const cancelTournamentRequest = catchAsyncErrors(async (req, res, next) => {
     .json({ success: true, message: "Tournament cancelled successfully" });
 });
 
+const deleteTournament = catchAsyncErrors(async (req, res, next) => {
+  const tournament_id = Number(req.params.tournament_id);
+
+  await prisma.tournament_referees.deleteMany({
+    where: {
+      tournament_id,
+    },
+  });
+
+  await prisma.tournament_sponsors.deleteMany({
+    where: {
+      tournament_id,
+    },
+  });
+
+  const tournament = await prisma.tournaments.delete({
+    where: {
+      id: tournament_id,
+    },
+  });
+
+  const user_tournaments = await prisma.tournaments.findMany({
+    where: {
+      user_id: tournament.user_id,
+    },
+  });
+
+  if (user_tournaments.length == 0) {
+    await prisma.users.update({
+      where: {
+        id: tournament.user_id,
+      },
+      data: {
+        is_organizer: false,
+      },
+    });
+  }
+
+  res
+    .status(200)
+    .json({ success: true, message: "Tournament deleted successfully" });
+});
+
+const deleteTeam = catchAsyncErrors(async (req, res, next) => {
+  const team_id = Number(req.params.team_id);
+
+  //checking if the team has participated in any tournament or not
+  const tournament_team_details = await prisma.tournament_teams.findFirst({
+    where: {
+      team_id,
+      is_selected: true,
+    },
+  });
+
+  if (tournament_team_details) {
+    return next(
+      new ErrorHandler("Can't delete the team participated in tournament")
+    );
+  }
+
+  //deleting the teams players
+  await prisma.team_players.deleteMany({
+    where: {
+      team_id,
+    },
+  });
+
+  //deleting the team
+  const team = await prisma.teams.delete({
+    where: {
+      id: team_id,
+    },
+  });
+
+  const user_teams = await prisma.teams.findMany({
+    where: {
+      user_id: team.user_id,
+    }
+  })
+
+  if(user_teams.length == 0){
+    await prisma.users.update({
+      where:{
+        id: team.user_id
+      },
+      data:{
+        is_manager: false
+      }
+    })
+  }
+
+  res.status(200).json({ success: true, message: "Team deleted successfully" });
+});
+
+const deletePlayer = catchAsyncErrors(async (req, res, next) => {
+  const player_id = Number(req.params.player_id);
+
+  //checking if the player has player any match
+  const player_stats = await prisma.player_statistics.findFirst({
+    where: {
+      player_id,
+      matches_player: {
+        gt: 0,
+      },
+    },
+  });
+
+  if (player_stats) {
+    return next(new ErrorHandler("Can't delete player who played a match"));
+  }
+
+  //Deleting the player stats
+  await prisma.player_statistics.delete({
+    where: {
+      player_id,
+    },
+  });
+
+  //deleting from the team
+  await prisma.team_players.deleteMany({
+    where: {
+      player_id,
+    },
+  });
+
+  //deleting the player details
+  const player = await prisma.players.delete({
+    where: {
+      id: player_id,
+    },
+  });
+
+  await prisma.users.update({
+    where: {
+      id: player.user_id,
+    },
+    data: {
+      is_player: false,
+    },
+  });
+
+  res
+    .status(200)
+    .json({ success: true, message: "Player deleted successfully" });
+});
+
 module.exports = {
   getTournamentRequest,
   approveTournamentRequest,
   cancelTournamentRequest,
+  deleteTournament,
+  deleteTeam,
+  deletePlayer,
 };
