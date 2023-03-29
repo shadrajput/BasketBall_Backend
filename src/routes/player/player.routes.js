69518e6beeeaab4817d5c8bd0225af3de6a4205a
shadrajput <shadrajput6@gmail.com>
2023-03-30 00:25:19 +0530

    Player details route

--- src/routes/player/player.routes.js
+++ src/routes/player/player.routes.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const { isAuthenticatedUser, isAuthPlayer } = require("../../middlewares/auth");
-const { playerRegistration,allPlayers,updatePlayerDetails,onePlayerDetailsbyId,onePlayerDetailsbyNumber,deletePlayerDetails } = require('./player.controller')
+const { playerRegistration, allPlayers, updatePlayerDetails, onePlayerDetailsbyId, onePlayerDetailsbyNumber, deletePlayerDetails } = require('./player.controller')
 const router = express.Router()
 
 router.post('/registration', isAuthenticatedUser, playerRegistration)
 router.get('/', isAuthenticatedUser, allPlayers)
-router.get('/:player_id', isAuthenticatedUser, onePlayerDetailsbyId)
-router.get('/:number', isAuthenticatedUser, onePlayerDetailsbyNumber)
-router.put(
-  "/update/:player_id",
+router.get('/details/:player_id', isAuthenticatedUser, onePlayerDetailsbyId)
+router.get('/details/:number', isAuthenticatedUser, onePlayerDetailsbyNumber)
+router.put("/update/:player_id",
   isAuthenticatedUser,
   isAuthPlayer, updatePlayerDetails
 );
