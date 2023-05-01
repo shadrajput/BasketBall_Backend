11578179fb3310f40a2196602c4ef5f3fa885fdb
SadikAli-Karadiya <karadiyasadikali@gmail.com>
2023-05-01 18:34:36 +0530

    some changes in gallery

--- src/routes/gallery/gallery.controller.js
+++ src/routes/gallery/gallery.controller.js
@@ -69,7 +69,7 @@ const adminGallery = catchAsyncErrors(async (req, res, next) => {
 
     res.status(200).json({
         data: allImages,
-        pageCount: totalImages / itemsPerPage,
+        pageCount: Math.ceil(totalImages / itemsPerPage),
         success: true,
     })
 })
@@ -94,7 +94,7 @@ const allGallery =  catchAsyncErrors(async (req, res, next) => {
 
     res.status(200).json({
         data: allImages,
-        pageCount: totalImages / itemsPerPage,
+        pageCount: Math.ceil(totalImages / itemsPerPage),
         success: true,
         message: "All Gallery"
     })
