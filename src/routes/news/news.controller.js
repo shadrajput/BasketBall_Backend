123a8f1de9707e72edc60cf52d1b9fae0ad6248a
shadrajput <shadrajput6@gmail.com>
2023-05-23 00:43:38 +0530

    EMIS

--- src/routes/news/news.controller.js
+++ src/routes/news/news.controller.js
@@ -16,7 +16,7 @@ const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
     urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
-
+`    `
 
 // ----------------------------------------------------
 // --------------------- Add --------------------------
